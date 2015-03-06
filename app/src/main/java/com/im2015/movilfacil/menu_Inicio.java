4b461053c865c79cab5504914ca34296c35071e5
CarlosRuizAscacibar <ruizcarlos93@yahoo.es>
2015-03-06 23:19:16 +0100

    Enlazado el botón de contactos con mensajes cambiarlo!!!

--- app/src/main/java/com/im2015/movilfacil/menu_Inicio.java
+++ app/src/main/java/com/im2015/movilfacil/menu_Inicio.java
@@ -1,9 +1,12 @@
 package com.im2015.movilfacil;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class menu_Inicio extends ActionBarActivity {
@@ -12,9 +15,15 @@ public class menu_Inicio extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu__inicio);
-    }
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener(){
 
-
+            public void onClick(View v){
+                Intent i = new Intent(v.getContext(),MenuMensajes.class);
+                startActivity(i);
+            }
+        });
+        }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
